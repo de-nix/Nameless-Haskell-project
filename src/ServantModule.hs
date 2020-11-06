55fdf821e63a3d76bbe7fcf8d86bc4e983fd6a87
99criste15 <criste.denis15@yahoo.com>
2020-11-06 19:00:15 +0200

    servant api

--- src/ServantModule.hs
+++ src/ServantModule.hs
@@ -11,18 +11,40 @@ import JSON
 import Data.Proxy
 import Network.Wai.Handler.Warp
 
-type API = "student" :> Capture "id" Int :> Get '[JSON] Student
-       -- :<|> "attendance" :> Capture "id" Int :> Get '[JSON] Attendance
-       -- :<|> "student" :> ReqBody '[JSON] Student :> Post '[JSON] () 
+import Data.Pool (Pool, withResource)
 
-server :: Server API
-server = getStudent
 
-getStudent :: Int -> Handler Student
-getStudent x = liftIO$ fromMaybe (Student (-1) "" "" "")<$> findStudent (toInteger x) (Connection "input.in") 
 
-proxyAPI :: Proxy API
-proxyAPI = Proxy
-app :: Application
-app = serve proxyAPI server
-runServer = run 8081 app
+
+app' :: Connection -> Application
+app' conn = serve api $ hoistServer api turnToHandler $ (getStudent conn) :<|>flip createStudent conn :<|> flip updateStudent conn:<|> flip removeStudent conn:<|> getAttendance conn :<|>flip createAttendance conn:<|>flip updateAttendance conn:<|>flip removeAttendance conn
+
+
+
+turnToHandler :: IO x -> Handler x
+turnToHandler val = liftIO val
+
+
+	
+
+
+getStudent :: (ModelAPI a m, Monad m) => a -> StudentId -> m Student
+getStudent conn sID = fromMaybe (Student (-1) "" "" "") <$> findStudent sID conn 
+
+getAttendance :: (ModelAPI a m, Monad m) => a -> AttendanceId -> m Attendance
+getAttendance conn sID = fromMaybe (Attendance (-1) (-1) (-1) "" False) <$> findAttendance sID conn 
+
+type API = "student" :> Capture "id" Integer :> Get '[JSON] Student
+      :<|> "student" :> ReqBody '[JSON] Student :> Post '[JSON] () 
+      :<|> "student" :> ReqBody '[JSON] Student :> Put '[JSON] ()
+      :<|> "student" :> Capture "id" Integer :> Delete '[JSON] ()
+      :<|> "attendance" :> Capture "id" Integer :> Get '[JSON] Attendance
+      :<|> "attendance" :> ReqBody '[JSON] Attendance :> Post '[JSON] ()
+      :<|> "attendance" :> ReqBody '[JSON] Attendance :> Put '[JSON] ()
+      :<|> "attendance" :> Capture "id" Integer :> Delete '[JSON] ()
+
+
+api :: Proxy API
+api = Proxy
+
+runServer = run 8081 $ app' $ Connection "input.in"
